9ae92d2819fc053413ec0ab279d0d48e247b5ea9
k8cchaha <k8cchaha@gmail.com>
2024-11-16 20:02:11 +0800

    add live-server

--- start/use-ts.ts
+++ start/use-ts.ts
@@ -1,11 +1,11 @@
-const num1 = document.querySelector('#num1')! as HTMLInputElement;
-const num2 = document.querySelector('#num2')! as HTMLInputElement;
-const button = document.querySelector('button')! as HTMLButtonElement;
+const num1 = document.querySelector("#num1")! as HTMLInputElement;
+const num2 = document.querySelector("#num2")! as HTMLInputElement;
+const button = document.querySelector("button")! as HTMLButtonElement;
 
 function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-button.addEventListener('click', () => {
+button.addEventListener("click", () => {
   console.log(add(+num1.value, +num2.value));
 });
